63804fcef05e9de9c30f0454eee89863010c9adf
DmitryK <infosecoff@yandex.com>
2024-03-18 14:19:55 +0300

    add build and push

--- TrialApp/TrialApp/test.py
+++ TrialApp/TrialApp/test.py
@@ -1,14 +1,5 @@
-# from django.test import TestCase
-
-# # from . import models
-
-# class SimpleTest(TestCase):
-#     def test_first(self):
-#         self.assertEqual('Text 1', 'Text 1')
-# backend/api/tests.py
 from http import HTTPStatus
 
-# from api import models
 from django.test import Client, TestCase
 
 
@@ -17,13 +8,6 @@ class TestAdminStatus(TestCase):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """Проверка доступности админки."""
         response = self.guest_client.get('/admin/login/?next=/admin/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    # def test_task_creation(self):
-    #     """Проверка создания задачи."""
-    #     data = {'title': 'Test', 'description': 'Test'}
-    #     response = self.guest_client.post('/api/tasks/', data=data)
-    #     self.assertEqual(response.status_code, HTTPStatus.CREATED)
-    #     self.assertTrue(models.Task.objects.filter(title='Test').exists())
