bcffbcc4b16a4dd618f139d28dff1104fbf7d25c
DmitryK <infosecoff@yandex.com>
2024-06-15 12:21:29 +0300

    render2

--- TrialApp/TrialApp/settings.py
+++ TrialApp/TrialApp/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'banks.apps.BanksConfig'
+    'banks.apps.BanksConfig',
     'django.contrib.http'
 ]
 
