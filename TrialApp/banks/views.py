c05ff4d4d278c92fe27b277b3a065a56bf621577
DmitryK <infosecoff@yandex.com>
2024-06-15 13:11:53 +0300

    jpeg

--- TrialApp/banks/views.py
+++ TrialApp/banks/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponse
 
 
 def show_image(request):
-    # media_root = settings.MEDIA_ROOT
-    # image_path = os.path.join(media_root, '1.jpg')
     image_path = 'media/1.jpg'
     with open(image_path, 'rb') as f:
         response = HttpResponse(f.read(), content_type='image/jpeg')
-        # response['Content-Disposition'] = 'attachment; filename="1.jpg"',
         return response
