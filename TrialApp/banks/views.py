37a893e61da4e89e52020ead55c239aae40cd16a
DmitryK <infosecoff@yandex.com>
2024-06-16 10:38:22 +0300

    2.jpeg

--- TrialApp/banks/views.py
+++ TrialApp/banks/views.py
@@ -1,9 +1,8 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def show_image(request):
-    image_path = 'media/1.jpg'
+    image_path = 'media/2.jpg'
     with open(image_path, 'rb') as f:
         response = HttpResponse(f.read(), content_type='image/jpeg')
         return response
