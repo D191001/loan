9679bfc1b306162b336ca6fb2f4fee4b8f3ac4b1
DmitryK <infosecoff@yandex.com>
2024-09-19 00:10:40 +0300

    add index.html

--- TrialApp/banks/urls.py
+++ TrialApp/banks/urls.py
@@ -1,8 +1,16 @@
+# from django.urls import path
+
+# from . import views
+
+# urlpatterns = [
+#     # path('', views.index),
+#     path('', views.show_image, name='show_image'),
+# ]
 from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    # path('', views.index),
-    path('', views.show_image, name='show_image'),
+    path("", views.index),
 ]
--- TrialApp/banks/views.py
+++ TrialApp/banks/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
-def show_image(request):
-    image_path = 'media/2.jpg'
-    with open(image_path, 'rb') as f:
-        response = HttpResponse(f.read(), content_type='image/jpeg')
-        return response
+# def show_image(request):
+#     image_path = 'media/2.jpg'
+#     with open(image_path, 'rb') as f:
+#         response = HttpResponse(f.read(), content_type='image/jpeg')
+#         return response
+
+
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html")
