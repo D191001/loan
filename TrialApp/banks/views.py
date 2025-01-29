cd7ee79331c44f92da23c9bd4e1efd3f0f73a153
DmitryK <infosecoff@yandex.com>
2025-01-29 13:27:13 +0300

    restart

--- TrialApp/banks/views.py
+++ TrialApp/banks/views.py
@@ -1,13 +1,3 @@
-# from django.http import HttpResponse
-
-
-# def show_image(request):
-#     image_path = 'media/2.jpg'
-#     with open(image_path, 'rb') as f:
-#         response = HttpResponse(f.read(), content_type='image/jpeg')
-#         return response
-
-
 from django.shortcuts import render
 
 
